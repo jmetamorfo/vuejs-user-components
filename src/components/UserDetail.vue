7ab902f3733608d22f81ba031a808dbbeb20b792
Juan Munoz <juan.munoz@lemon42.com>
2017-05-27 23:04:34 -0700

    Add type validation for properties

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -8,7 +8,12 @@
 
 <script>
   export default {
-      props: ['myName'],
+      props: {
+        myName: {
+          type: String,
+          default: 'secret'
+        }
+      },
       methods: {
         switchName() {
           return this.myName.split("").reverse().join("");
